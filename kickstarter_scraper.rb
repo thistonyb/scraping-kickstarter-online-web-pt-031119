3996b94f51563ad8f15501d5c8b3fc768b575cb6
Tony Baker <this.tonybaker@gmail.com>
2019-04-16 21:38:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,9 +4,9 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
+  binding.pry
 end
-binding.pry
+
 create_project_hash
 
 # projects: kickstarter.css("li.project.grid_4")
