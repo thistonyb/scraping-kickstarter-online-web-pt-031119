c17f1dbbab511291e8ebc4e2b8101358ff300829
Tony Baker <this.tonybaker@gmail.com>
2019-04-16 22:28:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -24,7 +24,7 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
-
+    binding.pry
   end
   binding.pry
   #Return project hash
