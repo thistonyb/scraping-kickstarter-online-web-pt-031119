32ded673b09c4b6e5a535a2ec1a21cee78d66204
Tony Baker <this.tonybaker@gmail.com>
2019-04-16 21:54:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,3 +12,4 @@ binding.pry
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+#project.css("p.bbcard_blurb").text
